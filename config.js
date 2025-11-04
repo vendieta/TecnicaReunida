8caa6c03e5ae078150658a6fc7ba04136b46449a
vendieta <vendieta5@gmail.com>
2025-11-03 22:07:11 -0500

    Stop tracking config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// ⚠️ SOLO PARA DESARROLLO ⚠️
-
-import dotenv from "dotenv";
-dotenv.config();
-export const SUPABASE_URL = dotenv.SUPABASE_URL;
-export const SUPABASE_ANON_KEY = dotenv.SUPABASE_ANON_KEY; 
