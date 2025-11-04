56f0a58dd55daaf27305c6e6a36789d944676e6c
vendieta <vendieta5@gmail.com>
2025-11-03 22:03:09 -0500

    seguridad

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 // ⚠️ SOLO PARA DESARROLLO ⚠️
-export const SUPABASE_URL = "https://tbpwydamtenvfeseronl.supabase.co";
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRicHd5ZGFtdGVudmZlc2Vyb25sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE5MTY1NzAsImV4cCI6MjA3NzQ5MjU3MH0.6Lh5zH24eDMvS7G5erqfNEAv9Aq_pAhw_eQNYVBr-BI"; 
+
+import dotenv from "dotenv";
+dotenv.config();
+export const SUPABASE_URL = dotenv.SUPABASE_URL;
+export const SUPABASE_ANON_KEY = dotenv.SUPABASE_ANON_KEY; 
